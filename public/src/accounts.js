7837feee0e6b5125961d71af07f0d248796cb483
nszeto98 <96497095+nszeto98@users.noreply.github.com>
2022-01-09 18:53:59 -0800

    accounts.js and books.js updated

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,49 @@
-function findAccountById(accounts, id) {}
+// 2 parameters, accounts and ID
+// search accounts array using find() to match account.id to input id
+// return object with account information
+function findAccountById(accounts, id) {
+  let found = accounts.find((account) => account.id===id)
+  return found
+}
 
-function sortAccountsByLastName(accounts) {}
+// use sort() to arrange accounts array in order of last name
+// make sure all names are ordered properly by using .toLowerCase()
+// return accounts array
+function sortAccountsByLastName(accounts) {
+  accounts.sort((accA, accB) => accA.name.last.toLowerCase() > accB.name.last.toLowerCase() ? 1 : -1)
+  return accounts
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+// two parameters single account and books
+// see how many times that account ID appears in the books.borrows array and log to accumulator var
+// return accumulator var
+function getTotalNumberOfBorrows(account, books) {
+  let acc = 0
+  for (let i = 0; i<books.length; i++) {
+    for (let j = 0; j<books[i].borrows.length; j++) {
+      if (books[i].borrows[j].id === account.id)
+        acc ++
+    }
+  }
+  return acc
+} 
 
-function getBooksPossessedByAccount(account, books, authors) {}
+// 3 parameters, single account object, all books, all authors
+// loop through each book in books and see what books are item.returned === false
+// save non-returned books to empty taken var
+// forEach book in taken, we set author info to store the author with matching ID and set that to the book["authors"] object
+function getBooksPossessedByAccount(account, books, authors) {
+  let taken = [];
+  books.forEach((book) => {
+    if (book.borrows.find((item) => item.id===account.id && item.returned === false)) 
+      taken.push(book)
+  })
+  taken.forEach((book) => {
+    let authorInfo = authors.find((author) => author.id === book.authorId) 
+    book["author"] = authorInfo
+  })
+  return taken
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,32 @@
-function findAuthorById(authors, id) {}
+// simple find() on authors array to find what author has matching id as input
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id)
+}
 
-function findBookById(books, id) {}
+// find what books have the matching id as input id using .find() where book.id (array) === id (input)
+function findBookById(books, id) {
+  return books.find((book) => book.id === id)
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+// takes in book array to determine whether book is borrowed or returned
+// return array with two arrays inside
+// one array is all checked out books and other is all returned books
+function partitionBooksByBorrowedStatus(books) {
+  let borrowed = books.filter((book) => book.borrows.some((borrow) => !borrow.returned));
+  let returned = books.filter((book) => book.borrows.every((borrow) => borrow.returned));
+  let result = [[...borrowed], [...returned]] 
+  return result
+}
 
-function getBorrowersForBook(book, accounts) {}
+// use map() to see what books are being borrowed and then use find to see what accounts match the borrowed book's id
+// make sure list is only 10 long
+function getBorrowersForBook(book, accounts) {
+  return book.borrows.map((borrow) => {
+    let account = accounts.find((account) => account.id === borrow.id)
+    return {...borrow, ...account}
+  })
+  .slice(0,10)
+}
 
 module.exports = {
   findAuthorById,
